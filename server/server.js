ba4b59ed87c2f3a12536a42bcc1282b3f77d3bc6
Christian <j.christian.arredondo@gmail.com>
2017-07-26 19:06:57 -0500

    Remove port

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-var port = process.env.PORT || 1521;
+var port = process.env.PORT;
 // LISTENER
 app.listen(port, function () {
   console.log('Server is listening on port 1521')
